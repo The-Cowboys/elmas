50a18c4ec3d5007a3362cd423f34d67771de4d9b
Franco Sangiacomo <franconecat@gmail.com>
2024-03-06 18:19:04 +0100

    Enviar tonto a la api

---
+++ enviar_tonto_api.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+
+token = sys.argv[2]
+
+url = 'https://thecowboys.duckdns.org/api/tontos'
+
+def enviar_tonto_api(tonto):
+    id_tonto = tonto[2]
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+    data = {'id': id_tonto}
+
+    print(f"Enviando tonto a la API: {id_tonto}")
+
+    x = requests.post(url, json = data, headers = headers)
+
+    print(f"Response: {x.status_code}")
+    print(f"Response: {x.text}")
+
+
