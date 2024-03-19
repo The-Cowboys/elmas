8019540a64abc05fc63f83435f49d1762f14b643
Franco Sangiacomo <francosang@users.noreply.github.com>
2024-03-19 09:15:32 +0100

    Enviar tonto a la api (#2)

--- el_mas_tonto.py
+++ el_mas_tonto.py
@@ -5,10 +5,12 @@
 from correo import enviar_correo
 from leer_archivo import leer_archivo
 from tonto import encontrar_tonto
+from enviar_tonto_api import enviar_tonto_api
 
 
 usuarios = leer_archivo("usuarios.csv")
 correos = [usuario[1] for usuario in usuarios]
 print(correos)
 tonto = encontrar_tonto(usuarios)
+enviar_tonto_api(tonto)
 enviar_correo(correos, tonto[0])
